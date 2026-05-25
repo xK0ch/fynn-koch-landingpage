17636ccb7cb605a0da8010f7b070d010062df0e1
Fynn Koch <mail@fynn-koch.de>
2026-05-25 15:19:56 +0200

    Add icons

--- src/app/hero/hero.ts
+++ src/app/hero/hero.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-hero',
+  imports: [FaIconComponent],
   templateUrl: './hero.html',
   styleUrl: './hero.scss'
 })
-export class Hero {}
+export class Hero {
+  faGithub = faGithub;
+  faLinkedin = faLinkedin;
+}
--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -1,8 +1,29 @@
 import { Component } from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import {
+  faGlobe,
+  faCode,
+  faGear,
+  faPlane,
+  faMusic,
+  faGraduationCap,
+  faPuzzlePiece
+} from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-projects',
+  imports: [FaIconComponent],
   templateUrl: './projects.html',
   styleUrl: './projects.scss'
 })
-export class Projects {}
+export class Projects {
+  faMusic = faMusic;
+  faPlane = faPlane;
+  faGraduationCap = faGraduationCap;
+  faPuzzlePiece = faPuzzlePiece;
+  faGlobe = faGlobe;
+  faGear = faGear;
+  faCode = faCode;
+  faGithub = faGithub;
+}
